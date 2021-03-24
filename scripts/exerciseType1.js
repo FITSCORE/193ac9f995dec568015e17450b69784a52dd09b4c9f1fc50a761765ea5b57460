4a0692b753c286ade50701f715ff9763ecbb8535
vivekb47 <vivekbhosale_47@yahoo.co.in>
2021-03-24 14:31:27 +0530

    changes

--- scripts/exerciseType1.js
+++ scripts/exerciseType1.js
@@ -57,7 +57,7 @@ function setup(){
 
   //Camera Setup
   video = createCapture(VIDEO);
-  video.size(300,300);
+  video.size(400,400);
   video.parent('cameraHolder');
   //video.hide();
 
@@ -133,7 +133,7 @@ function gotResults(error, results){
       } else {
         startCounting.resetcount();
       }
-      updateStartStatus("Hold"+countSequence[0].toUpperCase()+" for "+startCounting.timeRemaining+"sec.")
+      updateStartStatus("Hold "+countSequence[0].toUpperCase()+" for "+startCounting.timeRemaining+"sec.")
     }
     changeSvg();
   } else {
